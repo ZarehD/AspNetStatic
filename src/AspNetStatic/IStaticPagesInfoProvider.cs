6ae0db25b1a89c894f3fb1845f32ae8da5c4bcc5
ZarehD <4337043+ZarehD@users.noreply.github.com>
2023-04-05 22:18:57 -0700

    Update name format

--- src/AspNetStatic/IStaticPagesInfoProvider.cs
+++ src/AspNetStatic/IStaticPagesInfoProvider.cs
@@ -1,5 +1,5 @@
 ﻿/*--------------------------------------------------------------------------------------------------------------------------------
-Copyright 2023 Zareh Dergevorkian
+Copyright 2023 Zareh DerGevorkian
 
 Licensed under the Apache License, Version 2.0 (the "License"); 
 you may not use this file except in compliance with the License. 
