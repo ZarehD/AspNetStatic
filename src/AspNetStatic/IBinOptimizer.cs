4671baf948f44fd78619273fde344658275eb1e6
Zareh D <4337043+ZarehD@users.noreply.github.com>
2025-01-01 19:35:21 -0800

    Update copyright year.

--- src/AspNetStatic/IBinOptimizer.cs
+++ src/AspNetStatic/IBinOptimizer.cs
@@ -1,5 +1,5 @@
 ﻿/*--------------------------------------------------------------------------------------------------------------------------------
-Copyright 2023-2024 Zareh DerGevorkian
+Copyright 2023-2025 Zareh DerGevorkian
 
 Licensed under the Apache License, Version 2.0 (the "License"); 
 you may not use this file except in compliance with the License. 
