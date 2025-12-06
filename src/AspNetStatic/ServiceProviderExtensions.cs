b5996433866f0b03a2d2970e0d4f6e1b1548cfb1
Stekeblad <12627375+Stekeblad@users.noreply.github.com>
2025-12-06 12:58:50 +0100

    Added file taht was missed in commit : "Added a new StaticResourcesInfoProviderBase implementation that at runtime finds all registered Razor Pages"

---
+++ src/AspNetStatic/ServiceProviderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+namespace AspNetStatic
+{
+	public static class ServiceProviderExtensions
+	{
+		public static IServiceCollection AddAspNetStatic(this IServiceCollection services)
+		{
+			services.AddHttpClient(Consts.AspNetStatic, client =>
+			{
+				client.DefaultRequestHeaders.Add(HeaderNames.UserAgent, Consts.AspNetStatic);
+			});
+
+			return services;
+		}
+	}
+}
