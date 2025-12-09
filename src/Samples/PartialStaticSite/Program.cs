80c54a8851a59def630ac13349eefb685f2cec29
Stekeblad <12627375+Stekeblad@users.noreply.github.com>
2025-12-09 19:10:37 +0100

    Added an example of how to register   in PartialStaticSite

--- src/Samples/PartialStaticSite/Program.cs
+++ src/Samples/PartialStaticSite/Program.cs
@@ -1,10 +1,50 @@
 #define ENABLE_STATIC_PAGE_FALLBACK
 
 using AspNetStatic;
+using AspNetStaticContrib;
 using AspNetStaticContrib.AspNetStatic;
-using Microsoft.Extensions.Hosting;
+using AspNetStaticContrib.Stekeblad;
 using PartialStaticSite;
 
+static void AddExampleSiteAspNetStatic(WebApplicationBuilder builder, int example)
+{
+	switch (example)
+	{
+		case 1:
+			// Example 1:
+			// Manually specify a list of pages and resources that should
+			// be included in the static site generation
+
+			builder.Services.AddAspNetStatic();
+			builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
+				new StaticResourcesInfoProvider(SampleStaticPages.GetCollection())
+				.Add(builder.Environment.GetWebRootCssResources(["**/site.css", "**/*.min.css"]))
+				.Add(builder.Environment.GetWebRootJsResources())
+				.Add(builder.Environment.GetWebRootBinResources(["**/*.ico"]))
+			);
+			break;
+		case 2:
+			// Example 2:
+			// Manually specify assets in wwwroot that should
+			// be included in the static site generation and register
+			// a second IStaticResourcesInfoProvider to discover Razor Pages at runtime
+
+			builder.Services.AddAspNetStatic()
+				.AddAspNetStaticContrib(); // Contrib required for ActionDescriptorPageResourceInfoProvider
+
+			builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
+				new StaticResourcesInfoProvider()
+				.Add(builder.Environment.GetWebRootCssResources(["**/site.css", "**/*.min.css"]))
+				.Add(builder.Environment.GetWebRootJsResources())
+				.Add(builder.Environment.GetWebRootBinResources(["**/*.ico"]))
+			);
+
+			builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
+			new ActionDescriptorPageResourceInfoProvider()
+			);
+			break;
+	}
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,12 +58,7 @@ builder.Services.AddRouting(
 
 builder.Services.AddRazorPages();
 
-builder.Services.AddSingleton<IStaticResourcesInfoProvider>(
-	new StaticResourcesInfoProvider(SampleStaticPages.GetCollection())
-	.Add(builder.Environment.GetWebRootCssResources(["**/site.css", "**/*.min.css"]))
-	.Add(builder.Environment.GetWebRootJsResources())
-	.Add(builder.Environment.GetWebRootBinResources(["**/*.ico"]))
-	);
+AddExampleSiteAspNetStatic(builder, example: 2);
 
 // Use the "no-ssg" arg to omit static file generation
 // during development (hot-reload, etc.)
