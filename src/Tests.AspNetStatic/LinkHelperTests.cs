63b52b018b862b94ced2ef72f60b274c6f1e84a1
Zareh D <4337043+ZarehD@users.noreply.github.com>
2026-03-10 00:48:28 -0700

    improve link-fixup regex.

--- src/Tests.AspNetStatic/LinkHelperTests.cs
+++ src/Tests.AspNetStatic/LinkHelperTests.cs
@@ -38,17 +38,22 @@ public class LinkHelperTests
 			new("/pages/page/rp1"),
 			new("/pages/page/rp2") { OutFile = File_Pages_Page_Rp2 },
 			new("/pages/page/rp2") { Query = "?p1=v1", OutFile = File_Pages_Page_Rp2_P1V1 },
-		]);
+        ]);
 
 
 	#region Fixup Href Values...
 
-	[DataTestMethod]
-	[DataRow(@"<a class=""abcd"" href=""/"" title=""test"">", @"<a class=""abcd"" href=""/index.html"" title=""test"">")]
+	[TestMethod]
+    [DataRow(@"<a class=""abcd"" href=""/"" title=""test"">", @"<a class=""abcd"" href=""/index.html"" title=""test"">")]
 	[DataRow(@"<a class=""abcd"" href=""/docs"" title=""test"">", @"<a class=""abcd"" href=""/docs/index.html"" title=""test"">")]
-	[DataRow(@"<a href="""" title=""test"">", @"<a href="""" title=""test"">")]
+    [DataRow(@"<a class=""abcd"" href=""/docs/page1"" title=""test"">", @"<a class=""abcd"" href=""/docs/page1.html"" title=""test"">")]
+    [DataRow(@"<a class=""abcd"" href=""/docs/page2"" title=""test"">", @"<a class=""abcd"" href=""/docs/page2/index.html"" title=""test"">")]
+    [DataRow(@"<a class=&quot;abcd&quot; href=""/docs"" title=""test"">", @"<a class=&quot;abcd&quot; href=""/docs/index.html"" title=""test"">")]
+    [DataRow(@"<a class=""&+-_;"" href=""/docs"" title=""test"">", @"<a class=""&+-_;"" href=""/docs/index.html"" title=""test"">")]
+    [DataRow(@"<a href="""" title=""test"">", @"<a href="""" title=""test"">")]
 	[DataRow(@"<a href=""/"" title=""test"">", @"<a href=""/index.html"" title=""test"">")]
-	[DataRow(
+    [DataRow(@"<a class=""abcd"" href=""/#features"" title=""test"">", @"<a class=""abcd"" href=""/index.html#features"" title=""test"">")]
+    [DataRow(
 		@"<a href=""/docs"" title=""test""> <div>abcd</div> <a href=""/docs"" title=""test"">",
 		@"<a href=""/docs/index.html"" title=""test""> <div>abcd</div> <a href=""/docs/index.html"" title=""test"">")]
 	[DataRow(@"<a href=""/docs/"" title=""test"">", @"<a href=""/docs/index.html"" title=""test"">")]
@@ -58,11 +63,11 @@ public class LinkHelperTests
 	[DataRow(@"<a href=""/blogs/blog/article2"">", @"<a href=""/blogs/blog/article2.html"">")]
 	[DataRow(@"<a href=""/blogs/blog/categories/"">", @"<a href=""/blogs/blog/categories/index.html"">")]
 	[DataRow(@"<a title=""blogs""
-   href=""/blog/"">", @"<a title=""blogs""
-   href=""/blog/index.html"">")]
+		href=""/blog/"">", @"<a title=""blogs""
+		href=""/blog/index.html"">")]
 	[DataRow(@"<a title=""blogs""
-   href=""/blog/article1"">", @"<a title=""blogs""
-   href=""/blog/article1.html"">")]
+		href=""/blog/article1"">", @"<a title=""blogs""
+		href=""/blog/article1.html"">")]
 	[DataRow(@"<a href=""/home/p1"">", $@"<a href=""/{Url_Home_P1}"">")]
 	[DataRow(@"<a href=""/home/p1/"">", $@"<a href=""/{Url_Home_P1}"">")]
 	[DataRow(@"<a href=""home/p1/"">", $@"<a href=""{Url_Home_P1}"">")]
@@ -72,14 +77,16 @@ public class LinkHelperTests
 	[DataRow(@"<a href=""pages/page/?p1=v1"">", @"<a href=""pages/page/index.html"">")]
 	[DataRow(@"<a href=""/pages/page/rp2"">", $@"<a href=""/{Url_Pages_Page_Rp2}"">")]
 	[DataRow(@"<a href=""/pages/page/rp2?p1=v1"">", $@"<a href=""/{Url_Pages_Page_Rp2_P1V1}"">")]
-	//[DataRow(@"<a href=""pages/page"">", @"<a href=""pages/page.htm"">")]
-	[DataRow(@"<area href=""/index.html""   alt=""test"">", @"<area href=""/index.html""   alt=""test"">")]
+    //[DataRow(@"<a href=""pages/page"">", @"<a href=""pages/page.htm"">")]
+    //-
+    [DataRow(@"<area href=""/index.html""   alt=""test"">", @"<area href=""/index.html""   alt=""test"">")]
 	[DataRow(@"<area href=""/index""   alt=""test"">", @"<area href=""/index""   alt=""test"">")]
 	[DataRow(@"<area href=""docs/"" alt=""test"">", @"<area href=""docs/index.html"" alt=""test"">")]
+	//-
 	[DataRow(@"<base href=""/docs/"">", @"<base href=""/docs/"">")]
 	[DataRow(@"<link rel=""abcd"" type=""efgh"" href=""/test"">", @"<link rel=""abcd"" type=""efgh"" href=""/test"">")]
-	//[DataRow(@"", @"")]
-	public void Test_FixupHrefValues(string source, string expected)
+    //[DataRow(@"", @"")]
+    public void Test_FixupHrefValues(string source, string expected)
 	{
 		var actual = source.FixupHrefValues(
 			_pages,
